19bd3758fb470988ce7b272c80dd1d44023e5d96
Ricard Mallafre <ricard.mallafre@basic-fit.com>
2022-06-01 10:19:57 +0200

    chore: added lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -17,9 +17,13 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 2,
+    '@typescript-eslint/explicit-module-boundary-types': 2,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/no-explicit-any': 1,
+    '@typescript-eslint/no-floating-promises': 2,
+    'consistent-return': 2,
+    'no-param-reassign': 2,
+    eqeqeq: 2,
   },
 };
