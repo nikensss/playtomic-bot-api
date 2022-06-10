520c52ae7bf54765916d29a51139dbd6ef8ce711
Ricard Mallafre <rikitzzz@gmail.com>
2022-06-10 23:54:11 +0200

    feat: added swagger

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { clsProxifyFastifyPlugin } from 'cls-proxify/integration/fastify';
 import { v4 as uuid } from 'uuid';
 import { PinoLoggerService } from './pino-logger-module/pino-logger/pino-logger.service';
 import { logger } from './pino-logger-module/pino-logger/pino-logger-cls';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -21,6 +22,15 @@ async function bootstrap(): Promise<void> {
   await app.register(clsProxifyFastifyPlugin, {
     proxify: ({ id, url }) => logger.child({ reqId: id, url }),
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest.js template')
+    .setDescription('The nest.js template API')
+    .setVersion('0.1.0')
+    .addTag('template')
+    .build();
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, doc);
 
   await app.listen(3000, '0.0.0.0');
 }
