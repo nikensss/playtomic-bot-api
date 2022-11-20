7abe9d4c67ded0c3b30f1be4818fc7d4e01b0dcf
Ricard Mallafre <rikitzzz@gmail.com>
2022-11-20 10:58:08 +0100

    feat: validate token and create user
    If a token is received and that user doesn't exist in the database, it
    will be created. All this comes from the fact that this API will not be
    exposed publicly and will be only available through localhost in the
    same machines as the telegram bot.

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,26 +1,56 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { DbService } from 'src/db/db.service';
+import { LoggerService } from 'src/logger/logger.service';
+import { z } from 'zod';
+
+const jwtPayloadValidator = z.object({
+  id: z.number(),
+  is_bot: z.boolean(),
+  first_name: z.string(),
+  last_name: z.string(),
+  username: z.string(),
+  language_code: z.string(),
+});
+
+type JwtPayload = z.infer<typeof jwtPayloadValidator>;
+
+const validatePayload = (payload: unknown): JwtPayload => jwtPayloadValidator.parse(payload);
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService, private db: DbService) {
+  constructor(config: ConfigService, private db: DbService, private logger: LoggerService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+    this.logger.setContext(this);
   }
 
-  async validate({ sub: id }: { sub: string; email: string }): Promise<Omit<User, 'hash'>> {
-    const user = await this.db.user.findUnique({
-      where: { id },
-      select: { id: true, email: true, createdAt: true, updatedAt: true, nonce: true },
-    });
-    if (!user) throw new ForbiddenException();
+  async validate(_payload: unknown): Promise<User> {
+    try {
+      const payload = validatePayload(_payload);
 
-    return user;
+      const user = await this.db.user.findUnique({
+        where: { telegramId: `${payload.id}` },
+      });
+
+      if (user) return user;
+
+      return await this.db.user.create({
+        data: {
+          telegramId: `${payload.id}`,
+          firstName: payload.first_name,
+          lastName: payload.last_name,
+          userName: payload.username,
+        },
+      });
+    } catch (ex) {
+      this.logger.error({ err: ex });
+      throw new UnauthorizedException();
+    }
   }
 }
